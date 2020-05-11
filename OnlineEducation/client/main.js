60226a6c3e62d6a7891d6d3998552bc7536cc8da
TheCmdrRex <coplienr@uwplatt.edu>
2020-05-11 01:26:19 -0500

    This is Assignment 5

--- OnlineEducation/client/main.js
+++ OnlineEducation/client/main.js
@@ -15,3 +15,36 @@ Router.route('/registrationNloginPage', function(){
 Router.route('/', function(){
     this.render('Home');
 });
+
+Template.registrationNloginPage.events({
+	'click #buttonsubmitRegistrationForm':function(event){
+		console.log("In button Registration");
+		console.log("inside registerEntity");
+		var selectTitle = document.getElementById('selectTitle').value;
+		var textFirstName = document.getElementById('textFirstName').value;
+		var textLastName = document.getElementById('textLastName').value;
+		var textUserName = document.getElementById('textUserName').value;
+		var passwordPassword = document.getElementById('passwordPassword').value;
+		var selectRegisterAs = document.getElementById('selectRegisterAs').value;
+		var textHighestDegree = document.getElementById('textHighestDegree').value;
+		var textCurrentOrganization = document.getElementById('textCurrentOrganization').value;
+		var textCurrentDesignation = document.getElementById('textCurrentDesignation').value;
+		var textEmail = document.getElementById('textEmail').value;
+		var textPhoneNumber = document.getElementById('textPhoneNumber').value;
+		var textareaOtherInfo = document.getElementById('textareaOtherInfo').value;
+		var selectCollection = "";
+		console.log(selectRegisterAs);
+		if(selectRegisterAs.toString() === "student") {console.log("Student selected");
+			selectCollection = "insert_collectionStudents";}
+		if(selectRegisterAs.toString() === "instructor") {console.log("Instructor selected");
+			selectCollection = "insert_collectionInstructors";}
+		console.log(selectCollection);
+		Meteor.call(selectCollection.toString(), textUserName, selectTitle, textFirstName, 
+			textLastName, passwordPassword, selectRegisterAs, textHighestDegree,
+			textCurrentOrganization, textCurrentDesignation, textEmail, textPhoneNumber, 
+			textareaOtherInfo, function(err, res) {
+				if (err) console.log("mongoDB Error");
+				else console.log("Registration Successful");
+			});
+	}
+});
--- OnlineEducation/server/main.js
+++ OnlineEducation/server/main.js
@@ -1,5 +1,39 @@
 import { Meteor } from 'meteor/meteor';
+//import { Mongo } from 'meteor/mongo';
 
-Meteor.startup(() => {
+Meteor.startup(function() {
   // code to run on server at startup
+  Meteor.methods({
+  	insert_collectionStudents: function(StudentId, StudentTitle, StudentFirstName, StudentLastName, StudentPassword, StudentHighestDegree, 
+  	StudentCurrentDesignation, StudentOrganization, StudentEmail, StudentPhone, StudentInterestArea, StudentOtherInfoToHighlight) {
+	  console.log("in insert_collectionStudents");
+	  var MongoClient = require('mongodb').MongoClient;
+
+	  var url = "mongodb://localhost:3001";
+	  MongoClient.connect(url, {useUnifiedTopology: true}), function(err, db) {
+  		if (err) throw err;
+  		console.log("Hello There");
+	  	var myobj = {
+	  		"StudentId": StudentId,
+	  		"StudentTitle": StudentTitle,
+	  		"StudentFirstName": StudentFirstName,
+	  		"StudentLastName": StudentLastName,
+	  		"StudentPassword": StudentPassword,
+	  		"StudentHighestDegree": StudentHighestDegree,
+	  		"StudentCurrentDesignation": StudentCurrentDesignation,
+	  		"StudentOrganization": StudentOrganization,
+	  		"StudentEmail": StudentEmail,
+	  		"StudentPhone": StudentPhone,
+	  		"StudentInterestArea": StudentInterestArea,
+	  		"StudentOtherInfoToHighlight": StudentOtherInfoToHighlight
+	  	}
+	  	var dbo = db.db("myOnlineDatabase")
+	  	dbo.collection("Students").insertOne(myobj, function(err, res) {
+	  		if (err) throw err;
+	  		console.log("1 record inserted");
+	  		db.close();
+	  	});
+	  }
+  	}
+  })
 });
