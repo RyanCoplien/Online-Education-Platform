2b91237b589d7c99c765ab013ee1669d861e4103
TheCmdrRex <coplienr@uwplatt.edu>
2020-05-11 01:46:05 -0500

    Assignment 5 finished

--- OnlineEducation/server/main.js
+++ OnlineEducation/server/main.js
@@ -8,11 +8,10 @@ Meteor.startup(function() {
   	StudentCurrentDesignation, StudentOrganization, StudentEmail, StudentPhone, StudentInterestArea, StudentOtherInfoToHighlight) {
 	  console.log("in insert_collectionStudents");
 	  var MongoClient = require('mongodb').MongoClient;
-
 	  var url = "mongodb://localhost:3001";
-	  MongoClient.connect(url, {useUnifiedTopology: true}), function(err, db) {
+	  MongoClient.connect(url, {useUnifiedTopology: true}, function(err, db) {
   		if (err) throw err;
-  		console.log("Hello There");
+  		console.log("Connected successfully to server");
 	  	var myobj = {
 	  		"StudentId": StudentId,
 	  		"StudentTitle": StudentTitle,
@@ -33,7 +32,7 @@ Meteor.startup(function() {
 	  		console.log("1 record inserted");
 	  		db.close();
 	  	});
-	  }
+	  });
   	}
   })
 });
