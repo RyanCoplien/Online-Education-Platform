f4e120dff767825f2f479dece9f1cfb48b91fc3c
TheCmdrRex <coplienr@uwplatt.edu>
2020-05-19 22:12:45 -0500

    The final exam content

--- OnlineEducation/client/main.js
+++ OnlineEducation/client/main.js
@@ -3,6 +3,7 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 import './registrationNlogin.html';
+import './queryDatabase.html';
 
 Router.route('Home', function(){
   this.render('Home');
@@ -10,6 +11,10 @@ Router.route('Home', function(){
 
 Router.route('/registrationNloginPage', function(){
    this.render('registrationNloginPage');
+});
+
+Router.route('/queryDatabase', function(){
+   this.render('queryDatabase');
 });
 
 Router.route('/', function(){
@@ -31,6 +36,7 @@ Template.registrationNloginPage.events({
 		var textCurrentDesignation = document.getElementById('textCurrentDesignation').value;
 		var textEmail = document.getElementById('textEmail').value;
 		var textPhoneNumber = document.getElementById('textPhoneNumber').value;
+		var textBirthDate = document.getElementById('textBirthDate').value;
 		var textareaOtherInfo = document.getElementById('textareaOtherInfo').value;
 		var selectCollection = "";
 		console.log(selectRegisterAs);
@@ -40,11 +46,25 @@ Template.registrationNloginPage.events({
 			selectCollection = "insert_collectionInstructors";}
 		console.log(selectCollection);
 		Meteor.call(selectCollection.toString(), textUserName, selectTitle, textFirstName, 
-			textLastName, passwordPassword, selectRegisterAs, textHighestDegree,
-			textCurrentOrganization, textCurrentDesignation, textEmail, textPhoneNumber, 
+			textLastName, passwordPassword, textHighestDegree, textCurrentDesignation, 
+			textCurrentOrganization, textEmail, textPhoneNumber, textBirthDate,
 			textareaOtherInfo, function(err, res) {
 				if (err) console.log("mongoDB Error");
 				else console.log("Registration Successful");
+			});
+	}
+});
+
+Template.queryDatabase.events({
+	'click #buttonsubmitqueryStudent':function(event){
+		console.log("In button Query Database");
+		console.log("inside queryEntity");
+		var textFirstName = document.getElementById('textFirstName').value;
+		Meteor.call("query_collectionStudents", textFirstName, function(err, res) {
+				if (err) console.log("mongoDB Error");
+				else console.log("Query Successful");
+				document.getElementById('textareaOtherInfo').value = "Students named " + textFirstName + 
+				" have birthdays on:" + "\n" + res;
 			});
 	}
 });
--- OnlineEducation/server/main.js
+++ OnlineEducation/server/main.js
@@ -3,9 +3,10 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(function() {
   // code to run on server at startup
+  var Future = Npm.require("fibers/future");
   Meteor.methods({
   	insert_collectionStudents: function(StudentId, StudentTitle, StudentFirstName, StudentLastName, StudentPassword, StudentHighestDegree, 
-  	StudentCurrentDesignation, StudentOrganization, StudentEmail, StudentPhone, StudentInterestArea, StudentOtherInfoToHighlight) {
+  	StudentCurrentDesignation, StudentOrganization, StudentEmail, StudentPhone, StudentBirthDate, StudentOtherInfoToHighlight) {
 	  console.log("in insert_collectionStudents");
 	  var MongoClient = require('mongodb').MongoClient;
 	  var url = "mongodb://localhost:3001";
@@ -23,7 +24,7 @@ Meteor.startup(function() {
 	  		"StudentOrganization": StudentOrganization,
 	  		"StudentEmail": StudentEmail,
 	  		"StudentPhone": StudentPhone,
-	  		"StudentInterestArea": StudentInterestArea,
+	  		"StudentBirthDate": StudentBirthDate,
 	  		"StudentOtherInfoToHighlight": StudentOtherInfoToHighlight
 	  	}
 	  	var dbo = db.db("myOnlineDatabase")
@@ -33,6 +34,33 @@ Meteor.startup(function() {
 	  		db.close();
 	  	});
 	  });
+  	},
+
+  	query_collectionStudents: function(StudentFirstName) {
+	  console.log("in query_collectionStudents");
+	  var MongoClient = require('mongodb').MongoClient;
+	  var url = "mongodb://localhost:3001";
+	  var future = new Future();
+	  MongoClient.connect(url, {useUnifiedTopology: true}, function(err, db) {
+  		if (err) throw err;
+  		console.log("Connected successfully to server");
+	  	var myobj = {
+	  		"StudentFirstName": StudentFirstName,
+	  	}
+	  	var dbo = db.db("myOnlineDatabase")
+	  	dbo.collection("Students").find(myobj).toArray(function(err, result) {
+	  		if (err) throw err;
+	  		var output = "";
+	  		var i;
+				for (i = 0; i < result.length; i++) {
+				  output += result[i].StudentBirthDate + "\n";
+				}
+	  		console.log(output);
+	  		future.return(output);
+	  		db.close();
+	  	});
+	  });
+	  return future.wait();
   	}
   })
 });
